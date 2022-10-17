7fd1d0b22c7c4c2d78e1367c660b7526e1162e00
robertosiqueirajr <robertosiqueira.spa@gmail.com>
2022-10-17 18:02:24 -0300

    modificação contato

---
+++ app.js
@@ -0,0 +1,8 @@
+const btnMobile = document.getElementById('btn-mobile')
+
+const mobile = () => {
+  const nav = document.getElementById('nav')
+  nav.classList.toggle('active')
+}
+
+btnMobile.addEventListener('click', mobile)
